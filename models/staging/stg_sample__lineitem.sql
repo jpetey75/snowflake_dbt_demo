007a8c7a20c0570b865849652948e9dc2b473cb6
Jake Peterson <jake@popsql.com>
2023-09-28 01:29:32 +0000

    add first few sample models

---
+++ models/staging/stg_sample__lineitem.sql
@@ -0,0 +1,6 @@
+select 
+    concat(l_orderkey, '---', l_linenumber) as line_key,
+    l_orderkey as order_key,
+    l_quantity as line_quantity
+
+from {{ source('sample', 'lineitem') }}
---
+++ models/staging/stg_sample__orders.sql
@@ -0,0 +1,19 @@
+select 
+	--primary key 
+    o_orderkey as order_key,	
+
+	--order details
+    o_totalprice as order_total,
+    o_orderdate as order_date,
+
+	--truncate date to month for monthly reports
+    date_trunc('month', o_orderdate) as order_month,
+
+	--make statuses more readable 
+    case 
+        when o_orderstatus = 'O' then 'Open'
+        when o_orderstatus = 'F' then 'Filled'
+        when o_orderstatus = 'P' then 'Processing'
+        end as order_status
+
+from {{ source('sample', 'orders') }}
